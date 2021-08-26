4212ab8503de809fe50d5edd40238f5eeacd3acf
jeeveshmishra <jeevesh.mishra@nav.no>
2021-08-26 10:50:49 +0200

    TOB-1822 Korrigerte get metode som fungerer. Den er testet med curl kommando

--- src/main/kotlin/no/nav/sokos/pdl/proxy/api/PdlApi.kt
+++ src/main/kotlin/no/nav/sokos/pdl/proxy/api/PdlApi.kt
@@ -5,13 +5,14 @@ import io.ktor.http.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.util.*
 import no.nav.sokos.pdl.proxy.pdl.entities.Person
 import org.slf4j.LoggerFactory
 import java.lang.Exception
 
 private val LOGGER = LoggerFactory.getLogger("no.nav.sokos.pdl.proxy.api.PdlApi")
 
-fun Application.pdlApi(ident:String) {
+fun Application.pdlApi() {
     var person = Person("Nav Navnesen", "12345678910")
     routing {
         route("") {
@@ -19,14 +20,15 @@ fun Application.pdlApi(ident:String) {
                 LOGGER.info("du er på post!")
                 try {
                     person = call.receive<Person>()
-                }catch (ex :Exception){
+                } catch (ex :Exception) {
                     LOGGER.info("Feil!!", ex)
                 }
 
                 call.respondText("Person '$person' stored correctly", status = HttpStatusCode.Created)
             }
 
-            get("hent-person") {
+            get("hent-person/{ident}") {
+                val personIdent = call.parameters.getOrFail("ident")
                 LOGGER.info("du er her!")
                 call.respond (
                     person
