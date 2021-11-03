ac2330f6e76fd61fb42a8dcf5a8592a62223f28a
jeeveshmishra <jeevesh.mishra@nav.no>
2021-11-03 12:41:42 +0100

    Fjernet unødvnedig duplikat av HttpServer

--- src/main/kotlin/no/nav/sokos/pdl/proxy/Bootstrap.kt
+++ src/main/kotlin/no/nav/sokos/pdl/proxy/Bootstrap.kt
@@ -1,15 +1,11 @@
+package no.nav.sokos.pdl.proxy
+
 import com.expediagroup.graphql.client.ktor.GraphQLKtorClient
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import no.nav.sokos.ereg.proxy.api.installCommonFeatures
-import no.nav.sokos.ereg.proxy.api.naisApi
-import no.nav.sokos.pdl.proxy.Configuration
-import no.nav.sokos.pdl.proxy.api.pdlApi
-import no.nav.sokos.pdl.proxy.defaultHttpClient
 import no.nav.sokos.pdl.proxy.person.pdl.PdlService
 import no.nav.sokos.pdl.proxy.person.security.AccessTokenClient
 import java.net.URL
-import java.util.concurrent.TimeUnit
 import kotlin.properties.Delegates
 
 fun main() {
@@ -27,21 +23,6 @@ fun main() {
         appState.running = false
         httpServer.stop()
     })
-}
-
-class HttpServer(
-    appState: ApplicationState,
-    port: Int = 8080,
-    pdlService: PdlService
-) {
-    private val embeddedServer = embeddedServer(Netty, port) {
-        installCommonFeatures()
-        pdlApi(pdlService = pdlService)
-        naisApi({ appState.initialized }, { appState.running })
-    }
-
-    fun start() = embeddedServer.start()
-    fun stop() = embeddedServer.stop(5, 5, TimeUnit.SECONDS)
 }
 
 class ApplicationState(
--- src/main/kotlin/no/nav/sokos/pdl/proxy/HttpServer.kt
+++ src/main/kotlin/no/nav/sokos/pdl/proxy/HttpServer.kt
@@ -1,14 +1,12 @@
 package no.nav.sokos.pdl.proxy
 
-import ApplicationState
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.metrics.micrometer.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.engine.stop
-import io.ktor.server.netty.Netty
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics
 import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics
 import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics
@@ -18,22 +16,22 @@ import io.prometheus.client.exporter.common.TextFormat
 import no.nav.sokos.ereg.proxy.api.installCommonFeatures
 import no.nav.sokos.ereg.proxy.api.naisApi
 import no.nav.sokos.ereg.proxy.api.swaggerApi
-import no.nav.sokos.pdl.proxy.api.installSecurity
+import no.nav.sokos.pdl.proxy.api.pdlApi
 import no.nav.sokos.pdl.proxy.person.metrics.Metrics
-import no.nav.sokos.pdl.proxy.person.security.ApiSecurityService
-import no.nav.sokos.pdl.proxy.person.service.PdlProxyService
+import no.nav.sokos.pdl.proxy.person.pdl.PdlService
 import java.util.concurrent.TimeUnit
 
 
 class HttpServer(
     appState: ApplicationState,
-    appConfig: Configuration,
-    pdlProxyService: PdlProxyService,
-    apiSecurityService: ApiSecurityService,
-    port: Int = 8083,
+    //appConfig: Configuration,
+    pdlService: PdlService,
+    //apiSecurityService: ApiSecurityService,
+    port: Int = 8080,
 ) {
     private val embeddedServer = embeddedServer(Netty, port) {
-        installSecurity(apiSecurityService, appConfig, appConfig.useAuthentication)
+        //installSecurity(apiSecurityService, appConfig, appConfig.useAuthentication)
+        pdlApi(pdlService)
         installCommonFeatures()
         installMetrics()
         swaggerApi()
@@ -58,7 +56,7 @@ class HttpServer(
                 }
             }
         }
-
+    }
     fun start() = embeddedServer.start()
     fun stop() = embeddedServer.stop(5, 5, TimeUnit.SECONDS)
-}}
+}
