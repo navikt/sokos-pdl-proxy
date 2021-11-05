298b06eee7de9bd8bae765281b90675468012916
jeeveshmishra <jeevesh.mishra@nav.no>
2021-11-05 15:16:54 +0100

    Byttet get call til post fordi identer er sensitivt informasjon.

--- src/main/kotlin/no/nav/sokos/pdl/proxy/api/PdlApi.kt
+++ src/main/kotlin/no/nav/sokos/pdl/proxy/api/PdlApi.kt
@@ -5,11 +5,14 @@ package no.nav.sokos.pdl.proxy.api
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.http.HttpStatusCode
+import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.routing.route
 import io.ktor.routing.routing
 import io.ktor.util.getOrFail
+import no.nav.sokos.pdl.proxy.pdl.entities.HentPerson
 import no.nav.sokos.pdl.proxy.person.pdl.PdlService
 import org.slf4j.LoggerFactory
 
@@ -19,10 +22,10 @@ fun Application.pdlApi(pdlService: PdlService) {
     routing {
         route("") {
             //TODO - Get til Post pga sensitivt informasjon.
-            get("hent-person/{ident}") {
-                val personIdent = call.parameters.getOrFail("ident")
+            post("hent-person") {
+                val hentPerson: HentPerson = call.receive()
                 LOGGER.info("du er her!")
-                val person = pdlService.hentPerson(personIdent)
+                val person = pdlService.hentPerson(hentPerson.ident)
                 LOGGER.info("du er etter pdl inkalling!")
                 call.respond(HttpStatusCode.OK, person!!)
             }
---
+++ src/main/kotlin/no/nav/sokos/pdl/proxy/pdl/entities/HentPerson.kt
@@ -0,0 +1,5 @@
+package no.nav.sokos.pdl.proxy.pdl.entities
+
+data class HentPerson (
+    val ident : String
+    )
