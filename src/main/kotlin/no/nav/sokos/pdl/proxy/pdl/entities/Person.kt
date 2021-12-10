a9fb23996fc6732601b73bf40e3fc5f135536374
jeeveshmishra <jeevesh.mishra@nav.no>
2021-12-10 10:04:33 +0100

    forkortetNavn skal vaere en del av responsen.

--- src/main/kotlin/no/nav/sokos/pdl/proxy/pdl/entities/Person.kt
+++ src/main/kotlin/no/nav/sokos/pdl/proxy/pdl/entities/Person.kt
@@ -3,6 +3,7 @@ package no.nav.sokos.pdl.proxy.pdl.entities
 data class Person(
     val fornavn: String,
     val mellomnavn: String?,
-    val etternavn: String
+    val etternavn: String,
+    val forkortetNavn: String?
 )
 
