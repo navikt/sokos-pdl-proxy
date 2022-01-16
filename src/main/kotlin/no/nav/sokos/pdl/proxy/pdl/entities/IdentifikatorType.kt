461b42c147aea40f724a8a4c162ffb3a64019f5c
jeeveshmishra <jeevesh.mishra@nav.no>
2022-01-16 22:57:15 +0100

    AKTOR_ID blir lagt som en IdentifikatorType.

--- src/main/kotlin/no/nav/sokos/pdl/proxy/pdl/entities/IdentifikatorType.kt
+++ src/main/kotlin/no/nav/sokos/pdl/proxy/pdl/entities/IdentifikatorType.kt
@@ -5,13 +5,15 @@ import no.nav.person.pdl.aktor.v1.Type
 
 enum class IdentifikatorType(val id: Int) {
     FOLKEREGISTERIDENTIFIKATOR(1),
-    N_PID(2);
+    N_PID(2),
+    AKTOR_ID(3);
 
     companion object {
         fun fra(typeFraPDL: Type): IdentifikatorType {
             return when (typeFraPDL) {
                 Type.FOLKEREGISTERIDENTIFIKATOR -> FOLKEREGISTERIDENTIFIKATOR
                 Type.N_PID -> N_PID
+                Type.AKTOR_ID -> AKTOR_ID
                 else -> {
                     throw IllegalArgumentException("Finner ingen mapping for Type $typeFraPDL i kontoregister person")
                 }
@@ -22,6 +24,7 @@ enum class IdentifikatorType(val id: Int) {
             return when (typeFraPDL) {
                 IdentGruppe.FOLKEREGISTERIDENT -> FOLKEREGISTERIDENTIFIKATOR
                 IdentGruppe.NPID -> N_PID
+                IdentGruppe.AKTORID -> AKTOR_ID
                 else -> {
                     throw IllegalArgumentException("Finner ingen mapping for IdentGruppe $typeFraPDL i kontoregister person")
                 }
