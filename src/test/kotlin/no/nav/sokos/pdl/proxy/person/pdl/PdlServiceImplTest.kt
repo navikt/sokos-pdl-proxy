0ef5e8a95e8dff6207533effa98d35f49065b426
jeeveshmishra <jeevesh.mishra@nav.no>
2022-02-02 09:34:05 +0100

    Person navn attributes kan være null

---
+++ src/test/kotlin/no/nav/sokos/pdl/proxy/person/pdl/PdlServiceImplTest.kt
@@ -0,0 +1,19 @@
+package no.nav.sokos.pdl.proxy.person.pdl
+
+import org.junit.jupiter.api.Test
+import kotlin.test.Ignore
+
+internal class PdlServiceImplTest {
+
+    @Test
+    @Ignore
+    fun hentPerson() {
+        //var pdlService = PdlServiceImpl()
+    }
+
+    @Test
+    @Ignore
+    fun hentIdenterForPerson() {
+
+    }
+}
