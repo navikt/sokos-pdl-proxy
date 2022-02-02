d8cd634db2f698fb00dcd83f4e84eaa1dae2b279
jeeveshmishra <jeevesh.mishra@nav.no>
2022-02-02 09:04:19 +0100

    Person entity navn attributer kan være null.

--- src/main/kotlin/no/nav/sokos/pdl/proxy/pdl/entities/Person.kt
+++ src/main/kotlin/no/nav/sokos/pdl/proxy/pdl/entities/Person.kt
@@ -1,9 +1,9 @@
 package no.nav.sokos.pdl.proxy.pdl.entities
 
 data class Person(
-    val fornavn: String,
+    val fornavn: String?,
     val mellomnavn: String?,
-    val etternavn: String,
+    val etternavn: String?,
     val forkortetNavn: String?
 )
 
