5a0945d209613c8b0d33b48d23c51ac72d4e111b
jeeveshmishra <jeevesh.mishra@nav.no>
2022-02-02 08:55:17 +0100

    Person navn kan være null.

--- src/main/kotlin/no/nav/sokos/pdl/proxy/pdl/entities/PersonDetaljer.kt
+++ src/main/kotlin/no/nav/sokos/pdl/proxy/pdl/entities/PersonDetaljer.kt
@@ -2,8 +2,8 @@ package no.nav.sokos.pdl.proxy.pdl.entities
 
 data class PersonDetaljer (
     val identer: List<Ident>,
-    val fornavn: String,
+    val fornavn: String?,
     val mellomnavn: String?,
-    val etternavn: String,
+    val etternavn: String?,
     val forkortetNavn: String?,
 )
