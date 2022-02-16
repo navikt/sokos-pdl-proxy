83815cf254f38b2b17ce1d4d1f742b1a2e021305
jeeveshmishra <jeevesh.mishra@nav.no>
2022-02-16 10:54:47 +0100

    api service skal vare en parameter for HttpServer instans.

--- src/main/kotlin/no/nav/sokos/pdl/proxy/Bootstrap.kt
+++ src/main/kotlin/no/nav/sokos/pdl/proxy/Bootstrap.kt
@@ -3,6 +3,7 @@ package no.nav.sokos.pdl.proxy
 import com.expediagroup.graphql.client.ktor.GraphQLKtorClient
 import no.nav.sokos.pdl.proxy.person.pdl.PdlServiceImpl
 import no.nav.sokos.pdl.proxy.person.security.AccessTokenClient
+import no.nav.sokos.pdl.proxy.person.security.ApiSecurityService
 import java.net.URL
 import kotlin.properties.Delegates
 
@@ -11,7 +12,10 @@ fun main() {
     val appConfig = Configuration()
     val accessTokenClient = if(appConfig.useAuthentication) AccessTokenClient(appConfig.azureAdClint, defaultHttpClient) else null
     val pdlService = PdlServiceImpl(GraphQLKtorClient(URL(appConfig.pdlUrl), defaultHttpClient), appConfig.pdlUrl, accessTokenClient)
-    val httpServer = HttpServer(appState, pdlService = pdlService)
+    val securityService = ApiSecurityService(
+        appConfig.azureAdServer.apiAllowLists,
+        appConfig.azureAdServer.preAutorizedApps)
+    val httpServer = HttpServer(appState, appConfig, pdlService = pdlService, securityService)
 
     httpServer.start()
 
