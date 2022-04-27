17e07ea2043f6280f4441bdb0709d170b957e4df
Jeevesh Mishra <jeevesh.mishra@nav.no>
2022-04-27 12:46:07 +0200

    TOB-1935 Oppdaterte query med henting av oppholdsadresse og kontaktadresse

--- src/main/kotlin/no/nav/sokos/pdl/proxy/person/pdl/PdlServiceImpl.kt
+++ src/main/kotlin/no/nav/sokos/pdl/proxy/person/pdl/PdlServiceImpl.kt
@@ -80,12 +80,14 @@ class PdlServiceImpl (
 
             if (result.data?.hentPerson?.navn.isNullOrEmpty() == true){
                 logger.warn() { "Det har oppstått en feil ved henting av person fra pdl api - navn er empty" }
-                return Person("", "", "", "", null)
+                return Person("", "", "", "", null, null, null)
             }
 
             val bostedsadresse = result.data?.hentPerson?.bostedsadresse
+            val kontaktadresse = result.data?.hentPerson?.kontaktadresse
+            val oppholdsadresse = result.data?.hentPerson?.oppholdsadresse
             val person = result.data?.hentPerson?.navn?.map {
-                Person(it.fornavn, it.mellomnavn, it.etternavn, it.forkortetNavn, bostedsadresse)
+                Person(it.fornavn, it.mellomnavn, it.etternavn, it.forkortetNavn, bostedsadresse, kontaktadresse, oppholdsadresse)
             }?.first()
 
             return person
