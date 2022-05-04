2d05678bb7743bd934b1373055a7333798bd7f30
Jeevesh Mishra <jeevesh.mishra@nav.no>
2022-05-04 10:39:51 +0200

    TOB-1935 Startet med å bygge en test.
    Co-authored-by: Anders Flaten <anders.flaten@nav.no>

--- src/test/kotlin/no/nav/sokos/pdl/proxy/person/pdl/PdlServiceImplTest.kt
+++ src/test/kotlin/no/nav/sokos/pdl/proxy/person/pdl/PdlServiceImplTest.kt
@@ -9,18 +9,26 @@ import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.content.TextContent
 import io.ktor.http.headersOf
+import no.nav.sokos.pdl.proxy.exception.PdlApiException
 import no.nav.sokos.pdl.proxy.jsonClientConfiguration
 import no.nav.sokos.pdl.proxy.pdl.entities.PersonDetaljer
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import resourceToString
 import java.net.URL
+import kotlin.test.assertFailsWith
 import kotlin.test.assertNotNull
 
 internal class PdlServiceImplTest {
     @Test
-    fun hentPerson() {
+    fun `Vellykket hent av person identer og navn med adresser fra Pdl`() {
 
-        val mockkGraphQlClient: GraphQLKtorClient = GraphQLKtorClient(URL("http://0.0.0.0"), setupMockEngine(HttpStatusCode.OK))
+        val mockkGraphQlClient = GraphQLKtorClient(URL("http://0.0.0.0"),
+            setupMockEngine(
+                "hentIdenter_Success_Response.json",
+                "hentPerson_Success_Response.json",
+                HttpStatusCode.OK)
+        )
         val pdlUrl = "http://0.0.0.0"
         val pdlService = PdlServiceImpl(mockkGraphQlClient, pdlUrl, accessTokenClient = null)
 
@@ -29,20 +37,61 @@ internal class PdlServiceImplTest {
         assertNotNull(personDetaljer)
     }
 
+    @Test
+    fun `Finnes ikke person identer fra Pdl`() {
+        val mockkGraphQlClient = GraphQLKtorClient(URL("http://0.0.0.0"),
+            setupMockEngine(
+                "hentIdenter_fant_ikke_person_response.json",
+                "hentPerson_FantIkkePerson_Response.json",
+                HttpStatusCode.OK)
+        )
+        val pdlUrl = "http://0.0.0.0"
+        val pdlService = PdlServiceImpl(mockkGraphQlClient, pdlUrl, accessTokenClient = null)
+        assertFailsWith<PdlApiException>(
+            message = "Fant ikke person",
+            block = {
+                pdlService.hentPersonDetaljer("22334455667")
+            }
+        )
+    }
+    @Test
+    @Disabled
+    fun `Ikke authentisert å hente person identer fra Pdl`() {
+        val mockkGraphQlClient = GraphQLKtorClient(URL("http://0.0.0.0"),
+            setupMockEngine(
+                "hentIdenter_IkkeAuthentisert_Response.json",
+                "hentPerson_IkkeAuthentisert_Response.json",
+                HttpStatusCode.OK)
+        )
+        val pdlUrl = "http://0.0.0.0"
+        val pdlService = PdlServiceImpl(mockkGraphQlClient, pdlUrl, accessTokenClient = null)
+
+        val personDetaljer : PersonDetaljer? = pdlService.hentPersonDetaljer("22334455667")
+
+        assertNotNull(personDetaljer)
+    }
+
+
 }
 
-private fun setupMockEngine(statusCode: HttpStatusCode = HttpStatusCode.OK): HttpClient {
+
+
+private fun setupMockEngine(
+    hentIdenterResponseFilNavn: String,
+    hentPersonResponseFilNavn: String,
+    statusCode: HttpStatusCode = HttpStatusCode.OK
+): HttpClient {
     return HttpClient(MockEngine { request ->
         val body = request.body as TextContent
         if (body.text.contains("hentIdenter")) {
             respond(
-                content = resourceToString("Hent_Ident_Success.json"),
+                content = resourceToString(hentIdenterResponseFilNavn),
                 headers = headersOf("Content-Type", ContentType.Application.Json.toString()),
                 status = statusCode
             )
         } else {
             respond(
-                content = resourceToString("Hent_Person_Success.json"),
+                content = resourceToString(hentPersonResponseFilNavn),
                 headers = headersOf("Content-Type", ContentType.Application.Json.toString()),
                 status = statusCode)
         }
