8ad2d6b042a9b9e1ee8c37eed80fe77b81fe8b93
Jeevesh Mishra <jeevesh.mishra@nav.no>
2022-06-02 11:48:40 +0200

    Revert "TOB-1935 Prøver å fikse deserialiseringsfeil."
    This reverts commit cce18fa09cc6db7da0cc0f5a932cac0602d1bb23.

--- src/main/kotlin/no/nav/sokos/pdl/proxy/api/model/IdentifikatorType.kt
+++ src/main/kotlin/no/nav/sokos/pdl/proxy/api/model/IdentifikatorType.kt
@@ -1,7 +1,6 @@
 package no.nav.sokos.pdl.proxy.api.model
 
 import no.nav.pdl.enums.IdentGruppe
-import no.nav.person.pdl.aktor.v1.Type
 
 enum class IdentifikatorType {
     FOLKEREGISTERIDENTIFIKATOR,
@@ -9,17 +8,6 @@ enum class IdentifikatorType {
     AKTOR_ID;
 
     companion object {
-        fun fra(typeFraPDL: Type): IdentifikatorType {
-            return when (typeFraPDL) {
-                Type.FOLKEREGISTERIDENTIFIKATOR -> FOLKEREGISTERIDENTIFIKATOR
-                Type.N_PID -> N_PID
-                Type.AKTOR_ID -> AKTOR_ID
-                else -> {
-                    throw IllegalArgumentException("Finner ingen mapping for Type $typeFraPDL i pdl proxy")
-                }
-            }
-        }
-
         fun fra(typeFraPDL: IdentGruppe): IdentifikatorType {
             return when (typeFraPDL) {
                 IdentGruppe.FOLKEREGISTERIDENT -> FOLKEREGISTERIDENTIFIKATOR
