cce18fa09cc6db7da0cc0f5a932cac0602d1bb23
Jeevesh Mishra <jeevesh.mishra@nav.no>
2022-06-02 11:00:54 +0200

    TOB-1935 Prøver å fikse deserialiseringsfeil.
    Co-authored-by: Anders Flaten <anders.flaten@nav.no>
    Co-authored-by: Mudasar Cheema <mudasar.cheema@nav.no>

--- src/main/kotlin/no/nav/sokos/pdl/proxy/api/model/IdentifikatorType.kt
+++ src/main/kotlin/no/nav/sokos/pdl/proxy/api/model/IdentifikatorType.kt
@@ -1,6 +1,7 @@
 package no.nav.sokos.pdl.proxy.api.model
 
 import no.nav.pdl.enums.IdentGruppe
+import no.nav.person.pdl.aktor.v1.Type
 
 enum class IdentifikatorType {
     FOLKEREGISTERIDENTIFIKATOR,
@@ -8,6 +9,17 @@ enum class IdentifikatorType {
     AKTOR_ID;
 
     companion object {
+        fun fra(typeFraPDL: Type): IdentifikatorType {
+            return when (typeFraPDL) {
+                Type.FOLKEREGISTERIDENTIFIKATOR -> FOLKEREGISTERIDENTIFIKATOR
+                Type.N_PID -> N_PID
+                Type.AKTOR_ID -> AKTOR_ID
+                else -> {
+                    throw IllegalArgumentException("Finner ingen mapping for Type $typeFraPDL i pdl proxy")
+                }
+            }
+        }
+
         fun fra(typeFraPDL: IdentGruppe): IdentifikatorType {
             return when (typeFraPDL) {
                 IdentGruppe.FOLKEREGISTERIDENT -> FOLKEREGISTERIDENTIFIKATOR
