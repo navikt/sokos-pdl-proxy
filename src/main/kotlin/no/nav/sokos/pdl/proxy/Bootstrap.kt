708d54affa6c535c81f555fdd85d7ac891aa559b
Anders Flaten <anders.flaten@nav.no>
2022-09-29 15:09:58 +0200

    TOB-2399: Rydder opp
    Co-authored-by: Per Birger Eriksen <per.birger.eriksen@nav.no>

--- src/main/kotlin/no/nav/sokos/pdl/proxy/Bootstrap.kt
+++ src/main/kotlin/no/nav/sokos/pdl/proxy/Bootstrap.kt
@@ -2,7 +2,6 @@ package no.nav.sokos.pdl.proxy
 
 import com.expediagroup.graphql.client.ktor.GraphQLKtorClient
 import java.net.URL
-import mu.KotlinLogging
 import no.nav.sokos.pdl.proxy.config.Configuration
 import no.nav.sokos.pdl.proxy.config.httpClient
 import no.nav.sokos.pdl.proxy.pdl.PdlService
@@ -10,42 +9,34 @@ import no.nav.sokos.pdl.proxy.pdl.security.AccessTokenClient
 import no.nav.sokos.pdl.proxy.pdl.security.ApiSecurityService
 import no.nav.sokos.pdl.proxy.util.ApplicationState
 
-private val logger = KotlinLogging.logger {}
 const val SECURE_LOGGER_NAME = "secureLogger"
 
 fun main() {
-
-    try {
-        val applicationState = ApplicationState()
-        val configuration = Configuration()
-
-        val accessTokenClient =
-            if (configuration.useAuthentication) AccessTokenClient(
-                configuration.azureAdClint,
-                httpClient
-            ) else null
-
-        val pdlService =
-            PdlService(
-                GraphQLKtorClient(URL(configuration.pdlUrl), httpClient),
-                configuration.pdlUrl,
-                accessTokenClient
-            )
-        val apiSecurityService = ApiSecurityService(
-            configuration.azureAdServer.apiAllowLists,
-            configuration.azureAdServer.preAutorizedApps
+    val applicationState = ApplicationState()
+    val configuration = Configuration()
+    val accessTokenClient =
+        if (configuration.useAuthentication) AccessTokenClient(
+            configuration.azureAdClint,
+            httpClient
+        ) else null
+    val pdlService =
+        PdlService(
+            GraphQLKtorClient(URL(configuration.pdlUrl), httpClient),
+            configuration.pdlUrl,
+            accessTokenClient
         )
-        val httpServer = HttpServer(applicationState, configuration, pdlService = pdlService, apiSecurityService)
+    val apiSecurityService = ApiSecurityService(
+        configuration.azureAdServer.apiAllowLists,
+        configuration.azureAdServer.preAutorizedApps
+    )
+    val httpServer = HttpServer(applicationState, configuration, pdlService = pdlService, apiSecurityService)
 
-        applicationState.ready = true
+    applicationState.ready = true
 
-        Runtime.getRuntime().addShutdownHook(Thread {
-            applicationState.ready = false
-            httpServer.stop()
-        })
-        httpServer.start()
-    } catch (ex: Throwable) {
-        logger.error("Feil i oppstart", ex)
-        throw ex
-    }
+    Runtime.getRuntime().addShutdownHook(Thread {
+        applicationState.ready = false
+        httpServer.stop()
+    })
+    httpServer.start()
+
 }
