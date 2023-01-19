5289245f214a96c9dd8fff4068c9eba47c909365
jeeveshmishra <jeevesh.mishra@nav.no>
2023-01-19 13:07:08 +0100

    TOB-2681 Endret PersonDetaljer med logikk å bestemme  hvilket navn skal brukes ut av PDL og FREG kilder.

--- src/main/kotlin/no/nav/sokos/pdl/proxy/api/model/PersonDetaljer.kt
+++ src/main/kotlin/no/nav/sokos/pdl/proxy/api/model/PersonDetaljer.kt
@@ -1,9 +1,7 @@
 package no.nav.sokos.pdl.proxy.api.model
 
-import no.nav.pdl.hentperson.Bostedsadresse
-import no.nav.pdl.hentperson.Kontaktadresse
-import no.nav.pdl.hentperson.Oppholdsadresse
-import no.nav.pdl.hentperson.Person
+import no.nav.pdl.hentperson.*
+import java.text.SimpleDateFormat
 
 data class PersonDetaljer(
     val identer: List<Ident>,
@@ -16,16 +14,82 @@ data class PersonDetaljer(
     val oppholdsadresse: List<Oppholdsadresse>,
 ) {
     companion object {
-        fun fra(identer: List<Ident>, person: Person?) = PersonDetaljer(
-            identer,
-            person?.navn?.firstOrNull()?.fornavn,
-            person?.navn?.firstOrNull()?.mellomnavn,
-            person?.navn?.firstOrNull()?.etternavn,
-            person?.navn?.firstOrNull()?.forkortetNavn,
-            person?.bostedsadresse?.firstOrNull(),
-            person?.kontaktadresse.orEmpty(),
-            person?.oppholdsadresse.orEmpty(),
-        )
+        fun fra(identer: List<Ident>, person: Person?): PersonDetaljer {
+            if (person?.navn?.size == 1) {
+                return PersonDetaljer(
+                    identer,
+                    person.navn.firstOrNull()?.fornavn,
+                    person.navn.firstOrNull()?.mellomnavn,
+                    person.navn.firstOrNull()?.etternavn,
+                    person.navn.firstOrNull()?.forkortetNavn,
+                    person.bostedsadresse?.firstOrNull(),
+                    person.kontaktadresse.orEmpty(),
+                    person.oppholdsadresse.orEmpty(),
+                )
+            }
+
+            val navnFraPdlKilde : Navn? = person?.navn?.filter { navn -> navn.metadata.master.equals("PDL") }?.firstOrNull()
+            val navnFraFregKilde: Navn? = person?.navn?.filter { navn -> navn.metadata.master.equals("FREG") }?.firstOrNull()
+
+            if (null == navnFraPdlKilde?.gyldigFraOgMed || null == navnFraFregKilde?.gyldigFraOgMed) {
+                return PersonDetaljer(
+                    identer,
+                    navnFraPdlKilde?.fornavn,
+                    navnFraPdlKilde?.mellomnavn,
+                    navnFraPdlKilde?.etternavn,
+                    navnFraPdlKilde?.forkortetNavn,
+                    person?.bostedsadresse?.firstOrNull(),
+                    person?.kontaktadresse.orEmpty(),
+                    person?.oppholdsadresse.orEmpty(),
+                )
+            }
+            val dateFormater = SimpleDateFormat("yyyy.MM.dd HH:mm")
+            val endringDatoFraPdlKile = dateFormater.parse(navnFraPdlKilde.gyldigFraOgMed)
+            val endringDatoFraFregKilde = dateFormater.parse(navnFraFregKilde.gyldigFraOgMed)
+
+            val dateComparison = endringDatoFraPdlKile.compareTo(endringDatoFraFregKilde)
+
+            when {
+                dateComparison > 0 -> {
+                    return PersonDetaljer(
+                        identer,
+                        navnFraPdlKilde.fornavn,
+                        navnFraPdlKilde.mellomnavn,
+                        navnFraPdlKilde.etternavn,
+                        navnFraPdlKilde.forkortetNavn,
+                        person.bostedsadresse?.firstOrNull(),
+                        person.kontaktadresse.orEmpty(),
+                        person.oppholdsadresse.orEmpty(),
+                    )
+                }
+
+                dateComparison < 0 -> {
+                    return PersonDetaljer(
+                        identer,
+                        navnFraFregKilde.fornavn,
+                        navnFraFregKilde.mellomnavn,
+                        navnFraFregKilde.etternavn,
+                        navnFraFregKilde.forkortetNavn,
+                        person.bostedsadresse?.firstOrNull(),
+                        person.kontaktadresse.orEmpty(),
+                        person.oppholdsadresse.orEmpty(),
+                    )
+                }
+
+                else -> {
+                    return PersonDetaljer(
+                        identer,
+                        navnFraPdlKilde.fornavn,
+                        navnFraPdlKilde.mellomnavn,
+                        navnFraPdlKilde.etternavn,
+                        navnFraPdlKilde.forkortetNavn,
+                        person.bostedsadresse?.firstOrNull(),
+                        person.kontaktadresse.orEmpty(),
+                        person.oppholdsadresse.orEmpty(),
+                    )
+                }
+            }
+        }
     }
 }
 
