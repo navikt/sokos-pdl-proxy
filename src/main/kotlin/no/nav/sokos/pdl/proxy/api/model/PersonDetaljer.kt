ba60844bf50b7813d0e49fe1edf5b53e56d98ee2
Lars Hartvigsen <lars.hartvigsen@nav.no>
2023-05-22 15:29:13 +0200

    Fikser logikken så alle testene går grønt

--- src/main/kotlin/no/nav/sokos/pdl/proxy/api/model/PersonDetaljer.kt
+++ src/main/kotlin/no/nav/sokos/pdl/proxy/api/model/PersonDetaljer.kt
@@ -4,6 +4,7 @@ import no.nav.pdl.hentperson.Bostedsadresse
 import no.nav.pdl.hentperson.Kontaktadresse
 import no.nav.pdl.hentperson.Oppholdsadresse
 import no.nav.pdl.hentperson.Person
+import java.time.LocalDateTime
 
 data class PersonDetaljer(
     val identer: List<Ident>,
@@ -16,16 +17,22 @@ data class PersonDetaljer(
     val oppholdsadresse: List<Oppholdsadresse>,
 ) {
     companion object {
-        fun fra(identer: List<Ident>, person: Person?) = PersonDetaljer(
-            identer,
-            person?.navn?.firstOrNull()?.fornavn,
-            person?.navn?.firstOrNull()?.mellomnavn,
-            person?.navn?.firstOrNull()?.etternavn,
-            person?.navn?.firstOrNull()?.forkortetNavn,
-            person?.bostedsadresse?.firstOrNull(),
-            person?.kontaktadresse.orEmpty(),
-            person?.oppholdsadresse.orEmpty(),
-        )
+        fun fra(identer: List<Ident>, person: Person?): PersonDetaljer {
+
+            val sortedByDate = person?.navn?.sortedByDescending { it.metadata.endringer.maxOf { endring -> LocalDateTime.parse(endring.registrert) } }
+            val riktigNavn = sortedByDate?.partition { !it.metadata.historisk }?.toList()?.flatMap { it.toList() }?.firstOrNull()
+
+            return PersonDetaljer(
+                identer,
+                riktigNavn?.fornavn,
+                riktigNavn?.mellomnavn,
+                riktigNavn?.etternavn,
+                riktigNavn?.forkortetNavn,
+                person?.bostedsadresse?.firstOrNull(),
+                person?.kontaktadresse.orEmpty(),
+                person?.oppholdsadresse.orEmpty(),
+            )
+        }
     }
 }
 
