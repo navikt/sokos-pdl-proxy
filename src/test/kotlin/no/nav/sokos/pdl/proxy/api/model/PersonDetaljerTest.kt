3577fae1a53ab68c00ac20307d2bdcad68887653
Lars Hartvigsen <lars.hartvigsen@nav.no>
2023-05-23 15:51:34 +0200

    Laget også test for tomme lister av endringer

--- src/test/kotlin/no/nav/sokos/pdl/proxy/api/model/PersonDetaljerTest.kt
+++ src/test/kotlin/no/nav/sokos/pdl/proxy/api/model/PersonDetaljerTest.kt
@@ -30,7 +30,7 @@ class PersonDetaljerTest {
     }
 
     @Test
-    fun `Interessant - feil i dato feiler først når de sammenlignes`() {
+    fun `Sammenligning av navn med feil i dato feiler men ett navn går bra`() {
         assertDoesNotThrow {
             testPersondetaljer(
                 TestNavn(HISTORISK, "", "Feil What")
@@ -43,6 +43,44 @@ class PersonDetaljerTest {
             testPersondetaljer(
                 TestNavn(HISTORISK, "", "Feil What"),
                 TestNavn(HISTORISK, "0000-00-00T00:00:00", "Hva Skjer")
+            )
+        }
+    }
+
+    @Test
+    fun `Sammenligning av navn med tomme lister av endringer feiler men ett navn går bra`() {
+        assertDoesNotThrow {
+            PersonDetaljer.fra(
+                emptyList(),
+                Person(
+                    listOf(
+                        Navn(
+                            fornavn = "Ingen",
+                            etternavn = "Endringer",
+                            metadata = Metadata(endringer = emptyList(), AKTIVT)
+                        )
+                    ), emptyList(), emptyList(), emptyList()
+                )
+            )
+        }
+        assertThrows<NoSuchElementException> {
+            PersonDetaljer.fra(
+                emptyList(),
+                Person(
+                    listOf(
+                        Navn(
+                            fornavn = "Ingen",
+                            etternavn = "Endringer",
+                            metadata = Metadata(endringer = emptyList(), AKTIVT)
+                        ),
+                        Navn(
+                            fornavn = "Heller",
+                            mellomnavn = "Ingen",
+                            etternavn = "Endringer",
+                            metadata = Metadata(endringer = emptyList(), AKTIVT)
+                        )
+                    ), emptyList(), emptyList(), emptyList()
+                )
             )
         }
     }
@@ -95,11 +133,13 @@ class PersonDetaljerTest {
     private fun testPersondetaljer(vararg testnavn: TestNavn) = PersonDetaljer.fra(emptyList(), testperson(*testnavn))
     private fun testperson(vararg navn: TestNavn) = Person(navn.map { navn(it) }, emptyList(), emptyList(), emptyList())
     private data class TestNavn(val historisk: Boolean, val endret: String, val navn: String)
+
     private fun navn(testNavn: TestNavn) = Navn(
         fornavn = testNavn.navn.split(" ").first(),
         etternavn = testNavn.navn.split(" ").last(),
         metadata = Metadata(endringer = listOf(Endring(registrert = testNavn.endret)), historisk = testNavn.historisk)
     )
+
     companion object {
         const val AKTIVT = false
         const val HISTORISK = true
