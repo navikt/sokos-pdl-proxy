db0876808b691bd4c57981511581046f9839cbfd
Lars Hartvigsen <lars.hartvigsen@nav.no>
2023-09-05 12:22:38 +0200

    Begrenser coadresse til 255 tegn

--- src/main/kotlin/no/nav/sokos/pdl/proxy/api/model/PersonDetaljer.kt
+++ src/main/kotlin/no/nav/sokos/pdl/proxy/api/model/PersonDetaljer.kt
@@ -1,18 +1,21 @@
 package no.nav.sokos.pdl.proxy.api.model
 
-import no.nav.pdl.hentperson.*
+import no.nav.pdl.hentperson.Bostedsadresse
+import no.nav.pdl.hentperson.Kontaktadresse
+import no.nav.pdl.hentperson.Oppholdsadresse
+import no.nav.pdl.hentperson.Person
 import no.nav.sokos.pdl.proxy.metrics.Metrics
 import java.time.LocalDateTime
 
 data class PersonDetaljer(
-        val identer: List<Ident>,
-        val fornavn: String?,
-        val mellomnavn: String?,
-        val etternavn: String?,
-        val forkortetNavn: String?,
-        val bostedsadresse: Bostedsadresse?,
-        val kontaktadresse: List<Kontaktadresse>,
-        val oppholdsadresse: List<Oppholdsadresse>,
+    val identer: List<Ident>,
+    val fornavn: String?,
+    val mellomnavn: String?,
+    val etternavn: String?,
+    val forkortetNavn: String?,
+    val bostedsadresse: Bostedsadresse?,
+    val kontaktadresse: List<Kontaktadresse>,
+    val oppholdsadresse: List<Oppholdsadresse>,
 ) {
     companion object {
         fun fra(identer: List<Ident>, person: Person?): PersonDetaljer {
@@ -23,19 +26,23 @@ data class PersonDetaljer(
             }
 
             val riktigNavn = navnList
-                    .sortedByDescending { it.metadata.endringer.maxOf { endring -> LocalDateTime.parse(endring.registrert) } }
-                    .partition { !it.metadata.historisk }.toList()
-                    .flatMap { it.toList() }.firstOrNull()
+                .sortedByDescending { it.metadata.endringer.maxOf { endring -> LocalDateTime.parse(endring.registrert) } }
+                .partition { !it.metadata.historisk }.toList()
+                .flatMap { it.toList() }.firstOrNull()
+
+            val maksAntallTegnBostedsadresse = person?.bostedsadresse?.firstOrNull()?.let {
+                it.copy(coAdressenavn = it.coAdressenavn?.take(255))
+            }
 
             return PersonDetaljer(
-                    identer,
-                    riktigNavn?.fornavn,
-                    riktigNavn?.mellomnavn,
-                    riktigNavn?.etternavn,
-                    riktigNavn?.forkortetNavn,
-                    person?.bostedsadresse?.firstOrNull(),
-                    person?.kontaktadresse.orEmpty(),
-                    person?.oppholdsadresse.orEmpty(),
+                identer,
+                riktigNavn?.fornavn,
+                riktigNavn?.mellomnavn,
+                riktigNavn?.etternavn,
+                riktigNavn?.forkortetNavn,
+                maksAntallTegnBostedsadresse,
+                person?.kontaktadresse.orEmpty(),
+                person?.oppholdsadresse.orEmpty(),
             )
         }
     }
