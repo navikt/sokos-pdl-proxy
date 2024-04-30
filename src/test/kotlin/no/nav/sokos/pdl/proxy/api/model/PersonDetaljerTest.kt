f5040b25bbf765f88895d08c4d13568aa96987f4
Lars Hartvigsen <lhartvik@gmail.com>
2024-04-30 11:45:21 +0200

    Endrer slik at PDL velges foran FREG, men kun dersom begge er aktive (#255)
    * Endrer slik at PDL velges foran FREG, men kun dersom begge er aktive
    
    * Fikser testdata som manglet master
    
    * Inliner tilbake variabel
    
    * Tester at man ikke velger seneste aktive navn når det er fra FREG, mens det finnes et eldre aktivt navn som er fra PDL
    
    * Litt mer skikkelige testdata
    
    * Bruker ikke historiske navn
    
    * Oppdaterer tester til å ikke ta høyde for historiske navn, som vi ikke lenger bestiller fra PDL

--- src/test/kotlin/no/nav/sokos/pdl/proxy/api/model/PersonDetaljerTest.kt
+++ src/test/kotlin/no/nav/sokos/pdl/proxy/api/model/PersonDetaljerTest.kt
@@ -2,14 +2,11 @@ package no.nav.sokos.pdl.proxy.api.model
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
-import java.time.format.DateTimeParseException
-import no.nav.pdl.hentperson.Endring
 import no.nav.pdl.hentperson.Metadata
 import no.nav.pdl.hentperson.Navn
 import no.nav.pdl.hentperson.Person
 import org.junit.jupiter.api.Assertions.assertDoesNotThrow
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 class PersonDetaljerTest {
 
@@ -29,23 +26,6 @@ class PersonDetaljerTest {
         ).isEqualTo("Aron")
     }
 
-    @Test
-    fun `Sammenligning av navn med feil i dato feiler men ett navn går bra`() {
-        assertDoesNotThrow {
-            testPersondetaljer(
-                TestNavn(HISTORISK, "", "Feil What")
-            )
-            testPersondetaljer(
-                TestNavn(HISTORISK, "0000-00-00T00:00:00", "Hva Skjer")
-            )
-        }
-        assertThrows<DateTimeParseException> {
-            testPersondetaljer(
-                TestNavn(HISTORISK, "", "Feil What"),
-                TestNavn(HISTORISK, "0000-00-00T00:00:00", "Hva Skjer")
-            )
-        }
-    }
 
     @Test
     fun `Sammenligning av navn med tomme lister av endringer feiler men ett navn går bra`() {
@@ -57,27 +37,7 @@ class PersonDetaljerTest {
                         Navn(
                             fornavn = "Ingen",
                             etternavn = "Endringer",
-                            metadata = Metadata(endringer = emptyList(), AKTIVT)
-                        )
-                    ), emptyList(), emptyList(), emptyList()
-                )
-            )
-        }
-        assertThrows<NoSuchElementException> {
-            PersonDetaljer.fra(
-                emptyList(),
-                Person(
-                    listOf(
-                        Navn(
-                            fornavn = "Ingen",
-                            etternavn = "Endringer",
-                            metadata = Metadata(endringer = emptyList(), AKTIVT)
-                        ),
-                        Navn(
-                            fornavn = "Heller",
-                            mellomnavn = "Ingen",
-                            etternavn = "Endringer",
-                            metadata = Metadata(endringer = emptyList(), AKTIVT)
+                            metadata = Metadata(PDL)
                         )
                     ), emptyList(), emptyList(), emptyList()
                 )
@@ -86,62 +46,49 @@ class PersonDetaljerTest {
     }
 
     @Test
-    fun `Skal velge senest endret navn når det er flere`() {
+    fun `Skal velge senest endret aktiv navn når det nyeste er historisk og overstyre PDL`() {
         assertThat(
             testPersondetaljer(
-                TestNavn(AKTIVT, endret = "2020-02-02T02:02:02", navn = "Aron Åberg"),
-                TestNavn(AKTIVT, endret = "2021-02-02T02:02:02", navn = "Berit Berger")
-            ).fornavn
-        ).isEqualTo("Berit")
-    }
-
-    @Test
-    fun `Skal velge senest endret aktiv navn når det nyeste er historisk`() {
-        assertThat(
-            testPersondetaljer(
-                TestNavn(AKTIVT, endret = "2020-02-02T02:02:02", navn = "Aron Åberg"),
-                TestNavn(HISTORISK, endret = "2021-02-02T02:02:02", navn = "Berit Berger")
+                TestNavn(AKTIVT, endret = "2020-02-02T02:02:02", navn = "Aron Åberg", master = PDL),
+                TestNavn(HISTORISK, endret = "2021-02-02T02:02:02", navn = "Berit Berger", master = FREG)
             ).fornavn
         ).isEqualTo("Aron")
     }
 
     @Test
-    fun `Skal velge senest endret historisk navn når det begge er historisk`() {
+    fun `Skal velge nyeste aktive navn fra PDL`() {
         assertThat(
             testPersondetaljer(
-                TestNavn(HISTORISK, endret = "2020-02-02T02:02:02", navn = "Aron Åberg"),
-                TestNavn(HISTORISK, endret = "2021-02-02T02:02:02", navn = "Berit Berger")
+                TestNavn(HISTORISK, endret = "2020-02-02T02:02:02", navn = "Aron Åberg", master = FREG),
+                TestNavn(HISTORISK, endret = "2021-02-02T02:02:02", navn = "Berit Berger", master = FREG),
+                TestNavn(HISTORISK, endret = "2022-02-02T02:02:02", navn = "Charlie Carlsson", master = FREG),
+                TestNavn(AKTIVT, endret = "2023-02-02T02:02:02", navn = "David Danestad", master = FREG),
+                TestNavn(AKTIVT, endret = "2024-02-02T02:02:02", navn = "Erik Eriksen", master = PDL),
+                TestNavn(AKTIVT, endret = "2025-02-02T02:02:02", navn = "Fredrik Fransen", master = FREG),
+                TestNavn(HISTORISK, endret = "2026-02-02T02:02:02", navn = "Gerd Grostad", master = PDL)
             ).fornavn
-        ).isEqualTo("Berit")
-    }
-
-    @Test
-    fun `Skal velge nyeste aktive navn`() {
-        assertThat(
-            testPersondetaljer(
-                TestNavn(HISTORISK, endret = "2020-02-02T02:02:02", navn = "Aron Åberg"),
-                TestNavn(HISTORISK, endret = "2021-02-02T02:02:02", navn = "Berit Berger"),
-                TestNavn(HISTORISK, endret = "2022-02-02T02:02:02", navn = "Charlie Carlsson"),
-                TestNavn(AKTIVT, endret = "2023-02-02T02:02:02", navn = "David Danestad"),
-                TestNavn(AKTIVT, endret = "2024-02-02T02:02:02", navn = "Erik Eriksen"),
-                TestNavn(AKTIVT, endret = "2025-02-02T02:02:02", navn = "Fredrik Fransen"),
-                TestNavn(HISTORISK, endret = "2026-02-02T02:02:02", navn = "Gerd Grostad")
-            ).fornavn
-        ).isEqualTo("Fredrik")
+        ).isEqualTo("Erik")
     }
 
     private fun testPersondetaljer(vararg testnavn: TestNavn) = PersonDetaljer.fra(emptyList(), testperson(*testnavn))
     private fun testperson(vararg navn: TestNavn) = Person(navn.map { navn(it) }, emptyList(), emptyList(), emptyList())
-    private data class TestNavn(val historisk: Boolean, val endret: String, val navn: String)
+    private data class TestNavn(
+        val historisk: Boolean,
+        val endret: String,
+        val navn: String,
+        val master: String = FREG
+    )
 
     private fun navn(testNavn: TestNavn) = Navn(
         fornavn = testNavn.navn.split(" ").first(),
         etternavn = testNavn.navn.split(" ").last(),
-        metadata = Metadata(endringer = listOf(Endring(registrert = testNavn.endret)), historisk = testNavn.historisk)
+        metadata = Metadata(master = testNavn.master)
     )
 
     companion object {
         const val AKTIVT = false
         const val HISTORISK = true
+        const val PDL = "PDL"
+        const val FREG = "FREG"
     }
 }
