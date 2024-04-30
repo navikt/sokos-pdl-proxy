9402ab322c54932766e88d6fe210a08d56a80d82
Lars Hartvigsen <lars.hartvigsen@nav.no>
2024-04-30 10:42:37 +0200

    Bruker ikke historiske navn

--- src/main/kotlin/no/nav/sokos/pdl/proxy/api/model/PersonDetaljer.kt
+++ src/main/kotlin/no/nav/sokos/pdl/proxy/api/model/PersonDetaljer.kt
@@ -1,6 +1,5 @@
 package no.nav.sokos.pdl.proxy.api.model
 
-import java.time.LocalDateTime
 import no.nav.pdl.hentperson.Bostedsadresse
 import no.nav.pdl.hentperson.Kontaktadresse
 import no.nav.pdl.hentperson.Oppholdsadresse
@@ -8,40 +7,35 @@ import no.nav.pdl.hentperson.Person
 import no.nav.sokos.pdl.proxy.metrics.Metrics
 
 data class PersonDetaljer(
-        val identer: List<Ident>,
-        val fornavn: String?,
-        val mellomnavn: String?,
-        val etternavn: String?,
-        val forkortetNavn: String?,
-        val bostedsadresse: Bostedsadresse?,
-        val kontaktadresse: List<Kontaktadresse>,
-        val oppholdsadresse: List<Oppholdsadresse>
+    val identer: List<Ident>,
+    val fornavn: String?,
+    val mellomnavn: String?,
+    val etternavn: String?,
+    val forkortetNavn: String?,
+    val bostedsadresse: Bostedsadresse?,
+    val kontaktadresse: List<Kontaktadresse>,
+    val oppholdsadresse: List<Oppholdsadresse>
 ) {
     companion object {
         fun fra(identer: List<Ident>, person: Person?): PersonDetaljer {
             val navnList = person?.navn ?: emptyList()
-            when (navnList.count { !it.metadata.historisk }) {
+            when (navnList.count()) {
                 0 -> Metrics.noAktivtNavnCounter.inc()
                 in 2..Int.MAX_VALUE -> Metrics.multipleAktiveNavnCounter.inc()
             }
 
-            val riktigNavn = navnList
-                    .sortedByDescending { it.metadata.endringer.maxOf { endring -> LocalDateTime.parse(endring.registrert) } }
-                    .partition { !it.metadata.historisk }.toList()
-                    .flatMap { it.toList() }
-                    .partition { it.metadata.master == "PDL" && !it.metadata.historisk}.toList()
-                    .flatMap { it.toList() }
-                    .firstOrNull()
+            val navnWithMasterPDL = navnList.find { it.metadata.master == "PDL" }
+            val navn = navnWithMasterPDL ?: navnList.firstOrNull()
 
             return PersonDetaljer(
-                    identer,
-                    riktigNavn?.fornavn,
-                    riktigNavn?.mellomnavn,
-                    riktigNavn?.etternavn,
-                    riktigNavn?.forkortetNavn,
-                    person?.bostedsadresse?.firstOrNull(),
-                    person?.kontaktadresse.orEmpty(),
-                    person?.oppholdsadresse.orEmpty(),
+                identer,
+                navn?.fornavn,
+                navn?.mellomnavn,
+                navn?.etternavn,
+                navn?.forkortetNavn,
+                person?.bostedsadresse?.firstOrNull(),
+                person?.kontaktadresse.orEmpty(),
+                person?.oppholdsadresse.orEmpty(),
             )
         }
     }
